3bc5d44ab85d7762c86cccd5a0f3ae0ab4eae6f1
fishcake9@gmail.com <fishcake9@gmail.com@ee544a35-fb4c-b864-3477-e4d42eb3f0bb>
2012-08-26 17:20:05 +0000

    Update for svn keywords

--- trunk/docs/resources/combined.js
+++ trunk/docs/resources/combined.js
@@ -121,7 +121,7 @@ jQuery.cookie = function(name, value, options) {
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  *
- * Revision: $Id: jquery.autocomplete.js 15 2009-08-22 10:30:27Z joern.zaefferer $
+ * Revision: $Id$
  */
 
 ;(function($) {
